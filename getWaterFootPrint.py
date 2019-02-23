51d50ad54da5648c91688c96e8ef71dc3b265f3b
Arami Guerra de la Llera <arami.g@outlook.com>
2019-02-23 17:29:53 -0600

    Have a function that look for the closest match

--- getWaterFootPrint.py
+++ getWaterFootPrint.py
@@ -1,5 +1,6 @@
 import json
-from pprint import pprint
+
+from fuzzywuzzy import fuzz
 
 # returnt he blue, green, grey, and total water footfrint of all the ingredients
 def waterFootPrint(ingredients):
@@ -31,12 +32,29 @@ def getIngreInfo(ingredient):
     with open('GlobalAvgWaterFootPrintForPrimaryCrop.json') as f:
         data = json.load(f)
 
-    
-    print (data[0][""]);
+    best = 0;
+    ttwf = 0
+    tgwf = 0
+    tbwf = 0
+    tgreywf = 0
+    bestM = ""
+
+    for x in data:
+        a = fuzz.ratio(x[0], ingredient)
+        if a > best:
+            best = a
+            bestM = x[0]
+            ttwf = x[1][3]
+            tgreywf = x[1][2]
+            tgwf = x[1][1]
+            tbwf = x[1][0]
+
+  
+    print (bestM)
 
     return 0;
 
-getIngreInfo("d")
+getIngreInfo("soy beans")
 
 
 
