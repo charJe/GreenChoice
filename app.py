35a898e21c165b150bda8d70fdfbf89f6edf6003
Arami Guerra de la Llera <arami.g@outlook.com>
2019-02-23 22:58:53 -0600

    stuff

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import json
+from process import *
+from getWaterFootPrint import *
 
-from flask import Flask
+from flask import Flask,request
 
 app = Flask(__name__)
 picture=None
@@ -12,10 +14,12 @@ def new():
         return "success"
 
 
-@app.route('/postPicture', methods=['POST'])
+@app.route('/postPicture/', methods=['POST'])
 def getPicture():
         global picture;
         if request.method=="POST":
                 picture=request.data
+                #print(picture)
+                OCR(picture)
 
         return "success"
